8e85338707a2cebce326a0559b4c5f13e03f9e22
flavionogueiraa <flavio.nogueira.profissional@gmail.com>
2023-10-17 13:48:40 -0300

    0.2.8

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="novadata_utils",
-    version="0.2.7",
+    version="0.2.8",
     url="https://github.com/TimeNovaData/novadata_utils/",
     license="MIT License",
     author="Flávio Silva",
